76f1b7f71d2e4e36ac054dbaa1aa202ed9149a9d
Michael Nelson <revnelson@gmail.com>
2018-11-18 16:59:42 +0800

    Add excercises from chapter 6 'Arrays'

---
+++ 6_arrays/6.rb
@@ -0,0 +1,9 @@
+names = ['bob', 'joe', 'susan', 'margaret']
+names['margaret'] = 'jody'
+
+TypeError: no implicit conversion of String into Integer
+  from (irb):2:in `[]='
+  from (irb):2
+  from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
+
+# In an Array, the key must be an index as an integer.
